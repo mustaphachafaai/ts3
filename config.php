899d2db5a0f01c0a095e65e75f7c4181e40930b3
mustaphachafaai <48251244+mustaphachafaai@users.noreply.github.com>
2019-03-22 23:45:00 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+/*	Server IP		*/						$ts3_ip = 'localhost';
+/*	Queryport 		*/						$ts3_queryport = 10011;
+/*	Port			*/						$ts3_port = 9987;	
+/*	Login Query		*/						$ts3_user = 'serveradmin';	
+/*	Password Query	*/						$ts3_pass = 'sbNdIvOZ';	
+/*	dg-Bot By Chinwi		*/						$bot_nickname = 'OnlineBot'; 
+/*	Channel to move	*/						$bot_move = 00;
+/*	#Sleep	"sec"	*/						$sleep = 60;	
+
+/*	Server Name		"1=On" "0=Off"  */		$Server = 1; 
+					/*	ServerName	*/			$server_name = "- [DG]  DriveGamer  [DG] -";
+	
+/*	Channel Name	"1=On" "0=Off"  */		$Channel = 1;
+					/*	Channel name*/			$channel_name = "[cspacer0][ User Online ] :  SooN!";
+					/*	Channel ID	*/			$channel_id = 330;
+				
+?>
